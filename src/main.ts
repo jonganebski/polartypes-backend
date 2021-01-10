8145071a6c8da1218d274dfcb944664e0b0e7c77
Jon Ganebski <jon.ganebski@gmail.com>
2021-01-10 13:33:38 +0900

    cors setup

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({ origin: 'http://localhost:3000' });
+  app.enableCors({
+    origin:
+      process.env.NODE_ENV === 'production'
+        ? 'https://polartypes.netlify.app'
+        : 'http://localhost:3000',
+  });
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
